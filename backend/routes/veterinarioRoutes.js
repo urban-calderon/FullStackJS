1a41496ed86ab55bd68fdfa02aa36722d313a21c
urbancalderon3 <urbancalderon3@gmail.com>
2023-05-12 16:36:05 -0600

    ADDED: vet route file

---
+++ backend/routes/veterinarioRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+import { registrar, perfil } from '../controllers/veterinarioController.js'
+
+router.post('/', registrar)
+router.get('/perfil', perfil)
+
+export default router;
