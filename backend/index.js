c9ce9b6d07708dac5339c9caba037f46546ef1c6
urbancalderon3 <urbancalderon3@gmail.com>
2023-06-02 14:50:53 -0600

    CHANGED: el puesrto del servidor y usar el metodo de express.json

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,15 @@ import conectarDB from './config/db.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js'
 
 const app = express()
+app.use(express.json())
+
 dotenv.config()
 
 conectarDB()
 
 app.use("/api/veterinarios", veterinarioRoutes)
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto: ${PORT}`)
