08b917dbd5175e31a33f571790f79a2647ae4a1c
urbancalderon3 <urbancalderon3@gmail.com>
2023-06-02 14:52:02 -0600

    ADDED: funcion de registrar veterinario y metodo para prevenir usuarios duplicados

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,10 +1,55 @@
+import Veterinario from '../models/Veterinario.js'
 
-const registrar = (req, res) => {
-    res.json({ msg: 'Registrando usuarios..'})
+const registrar = async (req, res) => {
+
+    const { email } = req.body;
+
+    // Prevenir usuarios duplicados
+    const existeUsuario = await Veterinario.findOne({ email });
+    if (existeUsuario) {
+        const error = new Error("Usuario ya registrado");
+        return res.status(400).json({ msg: error.message });
+    }
+
+    try {
+      // Guardar un Nuevo Veterinario
+      const veterinario = new Veterinario(req.body);
+      const veterinarioGuardado = await veterinario.save();
+    
+      res.json(veterinarioGuardado);
+      
+    } catch (error) {
+       console.log(error);
+    }
+
 }
+
 
 const perfil = (req, res) => {
     res.json({ msg: 'Mostrando perfil..'})
 }
 
-export { registrar, perfil }
+const confirmar = async (req, res) => {
+    const { token } = req.params
+
+    const usuarioConfirmar = await Veterinario.findOne({ token })
+
+    if(!usuarioConfirmar) {
+        const error = new Error('Token no váido')
+        return res.status(404).json({msg: error.message})
+    }
+
+    try {
+        usuarioConfirmar.token = null
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+
+        res.json({ msg: 'Usuario confirmado correctamente'})
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+}
+
+export { registrar, perfil, confirmar }
