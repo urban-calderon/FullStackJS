0db6ff367a0fe8f8af19c5b6cb3c6df35de62a16
urbancalderon3 <urbancalderon3@gmail.com>
2023-06-02 14:53:10 -0600

    ADDED: agregar token al modelo de veterinario

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,38 +1,40 @@
 import mongoose from "mongoose";
+import generarId from '../helpers/generarId.js'
 
 const veterinarioSchema = mongoose.Schema({
     nombre: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     telefono: {
-        type: String,
-        default: null,
-        trim: true,
+      type: String,
+      default: null,
+      trim: true,
     },
     web: {
-        type: String,
-        default: null,
+      type: String,
+      default: null,
     },
     token: {
-        type: String,
+      type: String,
+      default: generarId,
     },
     confirmado: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
-})
+});
 
-const Veterinario = mongoose.model("Veterinario", veterinarioSchema)
+const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
 export default Veterinario;
