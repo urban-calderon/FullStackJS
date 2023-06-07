42dfa99e78cb73264ac7eb7c9b0725f347913a36
urbancalderon3 <urbancalderon3@gmail.com>
2023-06-07 16:41:27 -0600

    ADDED: agregar los endpoints (CRUD) a las rutas de veterinario

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,9 +1,28 @@
 import express from 'express';
 const router = express.Router();
-import { registrar, perfil, confirmar } from '../controllers/veterinarioController.js'
+import { 
+    registrar, 
+    perfil, 
+    confirmar,
+    autenticar,
+    olvidePassword, 
+    comprobarToken,
+    nuevoPassword,
+} from '../controllers/veterinarioController.js'
+import checkAuth from '../middleware/authMiddleware.js'
 
+// Endpoints de área publica
 router.post('/', registrar)
-router.get('/perfil', perfil)
 router.get('/confirmar/:token', confirmar)
+router.post('/login', autenticar)
+router.post('/olvide-password', olvidePassword)
+router.get('/olvide-password/:token', comprobarToken)
+router.post('/olvide-password/:token', nuevoPassword)
+
+// otra forma de hacer la accion de get y post al mismo tiempo:
+//router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword)
+
+// Endpoints de area privada
+router.get('/perfil', checkAuth, perfil)
 
 export default router;
